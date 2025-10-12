7014d906295665a01c02ff55cffc3920d2e8758c
dynaman216 <dynaman216@gmail.com>
2025-10-12 09:54:55 -0400

    Part_05.7

--- WebClassTemp/Part_05/blog_frontend/src/components/Blog.jsx
+++ WebClassTemp/Part_05/blog_frontend/src/components/Blog.jsx
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
 
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState('show')
+
+  const ToggleShowDetails = () => {
+    if (showDetails == 'show') {
+      setShowDetails('hide')
+    } else {
+      setShowDetails('show')
+    }
+  }
+
+  const addLike = () => {
+
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      {blog.author}
+      <button onClick={ToggleShowDetails}>{showDetails}</button>
+      {showDetails === 'hide' && (
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes {blog.likes}
+            <button onClick={addLike}>Like</button>
+          </p>
+          {blog.user.name}
+        </div>
+      )}
+
+    </div>
+  )
+}
 export default Blog
