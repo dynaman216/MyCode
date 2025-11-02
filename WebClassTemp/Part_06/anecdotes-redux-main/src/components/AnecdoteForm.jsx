9b0b923e4648fe660688cdce156f8f497ef20666
dynaman216 <dynaman216@gmail.com>
2025-11-02 09:24:49 -0500

    Part_06.6

---
+++ WebClassTemp/Part_06/anecdotes-redux-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { useRef } from 'react'
+
+const AnecdoteForm = () => { 
+  const dispatch = useDispatch()
+  const newAnecdote = useRef()
+
+  const handleCreateAnecdote = (event) => {
+    event.preventDefault()
+    const content = newAnecdote.current.value
+    dispatch(createAnecdote(content))
+    newAnecdote.current.value = ''
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleCreateAnecdote}>
+        <div>
+          <input name="anecdote" ref={newAnecdote} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
---
+++ WebClassTemp/Part_06/anecdotes-redux-main/src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnectdoteList = () => { 
+  const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+  
+    const handleVote = (id) => {
+      dispatch(voteAnecdote(id))
+      console.log('vote', id)
+    }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes
+        .slice()
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnectdoteList
